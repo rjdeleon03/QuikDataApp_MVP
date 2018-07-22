850a88f187a2c9b28042cf95f1670890ad0c2271
Ricardo Pablo De Leon <ricardopablo.deleon@gmail.com>
2018-07-22 19:30:39 +0800

    Initial commit

---
+++ app/src/main/java/com/icred/quikdata/FormListActivity.java
@@ -0,0 +1,13 @@
+package com.icred.quikdata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_list);
+    }
+}
---
+++ app/src/main/java/com/icred/quikdata/FormListFragment.java
@@ -0,0 +1,29 @@
+package com.icred.quikdata;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FormListFragment extends Fragment {
+
+
+    public FormListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_form_list, container, false);
+    }
+
+}
