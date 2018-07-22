38b9c9662f1d05702657e7a3994832afcd91e0f8
DESKTOP-5FEF2FS <ricardopablo.deleon@gmail.com>
2018-07-22 22:49:46 +0800

    Setup form list views

---
+++ app/src/main/java/com/icred/quikdata/Modules/Base/IFragment.java
@@ -0,0 +1,6 @@
+package com.icred.quikdata.Modules.Base;
+
+public interface IFragment {
+
+    void onActivityCreated();
+}
--- app/src/main/java/com/icred/quikdata/Modules/FormList/FormListActivity.java
+++ app/src/main/java/com/icred/quikdata/Modules/FormList/FormListActivity.java
@@ -2,14 +2,24 @@ package com.icred.quikdata.Modules.FormList;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.icred.quikdata.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class FormListActivity extends AppCompatActivity {
+
+    @BindView(R.id.menu_toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_list);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
     }
 }
